c0c9f21c978487aedb45d014a74a62eb7680b925
someone-anonym <73366925+someone-anonym@users.noreply.github.com>
2020-10-27 01:06:11 +0400

    First Step

---
+++ taxSystem.py
@@ -0,0 +1,2 @@
+#This is a project which is created for calculating a simple taxing system of a district Government.
+#This project takes the user Income as input and calculates it according to the Goverment's rules and regulations.
